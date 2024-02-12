b3f51779fb0da4a7c61e1da41bd24935bf7c1323
maytinhdian <132795994+maytinhdian@users.noreply.github.com>
2024-02-12 16:53:27 +0700

    Edit page

--- src/Components/Edit/EditPage.jsx
+++ src/Components/Edit/EditPage.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./edit.css";
 const EditPage = () => {
+  const avaUrl = [
+   "https://preview.redd.it/w7rm34sdm8a71.png?width=587&format=png&auto=webp&s=986bd66673b1ae29e7f4532424396fcd46f3b72d",
+   "https://preview.redd.it/w7rm34sdm8a71.png?width=587&format=png&auto=webp&s=986bd66673b1ae29e7f4532424396fcd46f3b72d",
+   "https://preview.redd.it/w7rm34sdm8a71.png?width=587&format=png&auto=webp&s=986bd66673b1ae29e7f4532424396fcd46f3b72d",
+   "https://preview.redd.it/w7rm34sdm8a71.png?width=587&format=png&auto=webp&s=986bd66673b1ae29e7f4532424396fcd46f3b72d",
+   "https://preview.redd.it/w7rm34sdm8a71.png?width=587&format=png&auto=webp&s=986bd66673b1ae29e7f4532424396fcd46f3b72d",
+   "https://preview.redd.it/w7rm34sdm8a71.png?width=587&format=png&auto=webp&s=986bd66673b1ae29e7f4532424396fcd46f3b72d",
+   "https://preview.redd.it/w7rm34sdm8a71.png?width=587&format=png&auto=webp&s=986bd66673b1ae29e7f4532424396fcd46f3b72d",
+  ];
+  const [name,setName]=useState('Le Thanh Nha');
+  const [age,setAge]=useState(20);
+  const[about,setAbout]=useState("I'm cave");
+  const [theme,setTheme]=useState("#ff9051");
+  const[url,setUrl]=useState("https://preview.redd.it/w7rm34sdm8a71.png?width=587&format=png&auto=webp&s=986bd66673b1ae29e7f4532424396fcd46f3b72d")
   return (
     <>
       <form action="">
@@ -12,15 +26,30 @@ const EditPage = () => {
             <input
               type="text"
               name="userName"
-              id=""
+              id="1"
               placeholder="Le Thanh Nha"
+              onChange={(e)=>setName(e.target.value)}
             />
 
             <label htmlFor="age">Age</label>
-            <input type="text" name="age" id="" placeholder="20" />
+            <input type="text" name="age" id="2" placeholder="20" onChange={(e)=>setAge(e.target.value)} />
 
-            <label htmlFor="about">Age</label>
-            <textarea className="input-about" name="about" id="" />
+            <label htmlFor="about">About</label>
+            <textarea className="input-about" name="about" id="3" onChange={(e)=>setAbout(e.target.value)} />
+          </div>
+          <label htmlFor="">Profile Picture</label>
+          <div className="input-image-container">
+            {avaUrl.map((url) => {
+              return (
+                <>
+                  <img src={url} className="input-image" alt="" />
+                </>
+              );
+            })}
+          </div>
+          <div className="theme-container">
+            <label htmlFor="">Theme</label>
+            <input type="color" name="" className="theme-color" id="4" />
           </div>
         </section>
       </form>
